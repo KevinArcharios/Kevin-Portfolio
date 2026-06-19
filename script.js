31fea41969924c09c6a2e3bac6a42d4b3af5a3d9
KevinArcharios <kevinsimanjuntak7911@gmail.com>
2026-06-20 00:32:40 +0700

    Menambah skill section

---
+++ script.js
@@ -0,0 +1,38 @@
+const words = [
+	"Informatics Student",
+	"Front-End Developer",
+	"Creative Thinker",
+	"Lifelong Learner",
+];
+
+const text = document.getElementById("typing-text");
+
+let word = 0;
+let char = 0;
+let deleting = false;
+
+function type() {
+	const current = words[word];
+
+	text.textContent = current.slice(0, char);
+
+	if (!deleting) {
+		char++;
+
+		if (char > current.length) {
+			deleting = true;
+			return setTimeout(type, 1500);
+		}
+	} else {
+		char--;
+
+		if (char === 0) {
+			deleting = false;
+			word = (word + 1) % words.length;
+		}
+	}
+
+	setTimeout(type, deleting ? 50 : 100);
+}
+
+type();
